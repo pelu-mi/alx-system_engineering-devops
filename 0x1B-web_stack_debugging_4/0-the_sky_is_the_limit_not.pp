cf334e899782f39341942811c4de918dafddd017
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-07 07:20:52 +0100

    Fix 3 for nginx puppet script and script to fix holberton user

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Script to Fix failed requests on Nginx server
 
 exec { 'nginx-server-fix'
-  command => 'sed -i s/15/4096/ /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => ['/bin', '/usr/bin', '/usr/local/bin']
 }
 
