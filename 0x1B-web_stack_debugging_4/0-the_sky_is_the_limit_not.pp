d249f5f0c2f640d084502e4566b7a37aa3c23320
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-07 07:32:20 +0100

    Fix 5 for both files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Script to Fix failed requests on Nginx server
 
-exec { 'nginx-server-fix'
+exec { 'nginx-server-fix':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/bin:/usr/local/bin'
 }
 
--> exec { 'restart-nginx'
+-> exec { 'restart-nginx':
   command => 'nginx restart',
   path    => '/etc/init.d'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,11 @@
 # Puppet script to change OS config, allowing user to access files without error
 
-exec { 'fix-holberton-user':
-  command => 'sed -i "s/holberton/foo" /etc/security/limits.conf',
-  path    => '/usr/bin/env'
+exec { 'adjust-hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'adjust-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
