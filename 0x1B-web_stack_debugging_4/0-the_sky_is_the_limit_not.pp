f9c41abb3f4ecd72bed736a6f5922c6f0e56f7a9
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-07 07:10:18 +0100

    Fix 1 for nginx puppet script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,10 @@
 
 exec { 'nginx-server-fix'
   command => 'sed -i "s/15/4096/" /etc/default/nginx; sudo service nginx restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  path    => ['/bin', '/usr/bin', '/usr/local/bin']
+}
+
+-> exec { 'restart-nginx'
+  command => 'nginx restart',
+  path    => '/etc/init.d'
 }
